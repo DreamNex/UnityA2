9e6e594a4a193d51a7e1fc3877574c38d672c66f
DreamNex <dreamwalker56@gmail.com>
2016-01-10 22:28:53 +0800

    added 2 different scenes -SplashScreen -MainMenu

---
+++ IntefaceNDesign/Assets/Scripts/MenuScripts/SplashScreen_Transition.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen_Transition : MonoBehaviour {
+
+    float timeToChange = 1000;
+
+	// Use this for initialization
+	void Start () {
+
+         
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeToChange -= Time.deltaTime;
+
+        if(timeToChange <= 0)
+        {
+            //Change the scene to menu scene
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+	
+	}
+
+    public void ChangeToMenu()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+}
